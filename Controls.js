af5af16e1677b56aa7beffeeed62dc0fbc0fd1af
Filip Smets <alemaaltevinden@gmail.com>
2015-01-09 15:20:48 +0100

    Finished library

---
+++ Controls.js
@@ -0,0 +1,170 @@
+function Controls (container, controls) {
+	this.controls = controls || [];
+	this.byName = {};
+
+	this.buildControls();
+}
+
+// Removes everything from the container and
+// Creates the given controls, includes adding them to .byName
+Controls.prototype.buildControls = function buildControls () {
+	// Empty the container
+	while (container.firstChild) {
+		container.removeChild(container.firstChild);
+	}
+
+	// Create each controler from its definition
+	// Add to the container and .byName
+	for (var cKey = 0; cKey < this.controls.length; cKey++) {
+		var control = this.createControl(this.controls[cKey])
+		container.appendChild(control.containerAppend);
+		this.byName[this.controls[cKey].name] = control;
+	}
+};
+
+// CreateControl calls the constructor for the given control
+Controls.prototype.createControl = function createControl (control) {
+	if (typeof this.constructors[control.type] == "function") {
+		return this.constructors[control.type](control);
+	}
+
+	// Control is not defined
+	console.error("Unknown control: " + control.type, control);
+	return document.createTextNode("Unknown control: " + control.type);
+};
+
+// Object that holds all the contruction functions for the controllers
+// A construction object should return whatever should be stored in .byName["nameOfTheControl"]
+// The returned object should at least have:
+{
+	input: domElement,              //DomElement of which .value can be get and set,
+	containerAppend: domElement,    // Element that should be appended to the container
+}
+Controls.prototype.constructors = {};
+
+Controls.prototype.constructors.button = function createButton (control) {
+	var input = document.createElement("div");
+	input.className = (control.classAppend || "") + "control-button";
+
+	if (control.value)
+		input.value = control.value;
+
+	if (control.text)
+		input.appendChild(document.createTextNode(control.text));
+
+	if (control.image) {
+		var img = input.appendChild(document.createElement("img"));
+		img.src = control.image;
+		img.alt = control.alt;
+	}
+
+	if (control.title)
+		input.title = control.title;
+
+	input.addEventListener("click", function () {
+		control.action(input.value);
+	});
+
+	return {
+		input: input,
+		containerAppend: input
+	}
+};
+
+Controls.prototype.constructors.integer = function createIntegerInput (control) {
+	var container = document.createElement("div");
+	container.className = (control.classAppend || "") + "control-integer";
+
+	// Create the actual input field
+	var input = document.createElement("input");
+	input.type = "number";
+	input.value = control.value;
+	input.className = (control.classAppend || "") + "control-integer-input";
+
+	if (control.text)
+		input.placeholder = control.text;
+
+	if (control.title)
+		input.title = control.title;
+
+	if (control.max)
+		input.max = control.max;
+
+	if (control.min)
+		input.min = control.min;
+
+	input.addEventListener("input", function () {
+		control.action(input.value);
+	});
+
+	// Create the minus button
+	var minusButton = container.appendChild(this.button({
+		text: "-",
+		action: function () {
+			var max = parseInt(input.max || Infinity);
+			var nextValue = parseInt(input.value) - 1;
+			input.value = Math.min(max, nextValue);
+			control.action(input.value);
+		}
+	}).containerAppend);
+
+	// Append the input
+	container.appendChild(input);
+
+	// Create a plus button
+	container.appendChild(this.button({
+		text: "+",
+		action: function () {
+			var max = parseInt(input.max || Infinity);
+			var nextValue = parseInt(input.value) + 1;
+			input.value = Math.min(max, nextValue);
+			control.action(input.value);
+		}
+	}).containerAppend);
+
+};
+
+Controls.prototype.constructors.text = function createTextInput (control) {
+	// Create the actual input field
+	var input = document.createElement("input");
+	input.type = "text";
+	input.value = control.value;
+	input.className = (control.classAppend || "") + "control-text-input";
+
+	if (control.text)
+		input.placeholder = control.text;
+
+	if (control.title)
+		input.title = control.title;
+
+	if (control.max)
+		input.max = control.max;
+
+	if (control.min)
+		input.min = control.min;
+
+	input.addEventListener("input", function () {
+		control.action(input.value);
+	});
+};
+
+Controls.prototype.constructors.color = function createControlInput (control) {
+	// Create the actual input field
+	var input = document.createElement("input");
+	input.type = "text";
+	input.value = control.value;
+	input.className = (control.classAppend || "") + "control-color-input";
+
+	$(input).spectrum({
+		showAlpha: true,
+		showInput: true,
+		showInitial: true,
+		preferredFormat: "hex",
+		showPalette: true,
+		maxSelectionSize: 32,
+		clickoutFiresChange: true,		
+		move: function (color) {
+			control.action(color.toHexString());
+		}
+	});
+};
--- README.md
+++ README.md
@@ -18,19 +18,21 @@ The controls have been added and are ready to use. You can now use the controls 
 
 Example:
 
-	controls.byName.someName.input                // Input dom element; Can be a button, input element, ...
-	controls.byName.someName.setValue("value");   // Can be used to set a value of the control 
+	controls.byName.someName.input                // Input dom element; Can be a button, input element, ... has .value property that can be get and set (not guaranteed to invoke action)
 
 Control properties
 ==================
 
 		{
 			name: "someName",              // Required, string to use as name
-			type: "button",                // Possible types: button, text, integer or color
+			type: "button",                // Possible types: button, text, integer or color (hex)
 			text: "Click here"             // Used by some control types as button text or placeholder
+			classPrepend: "name-"          // A string that will be added to all classes, default ""
 			image: "link/to/image.png",    // If provided this image will be used instead of text
 			alt: "Image description",      // Will be used as image alt
 			value: "Value",                // The current value
-			action: function () {}         // Function that will be called with the value when status change (button press, stopped typing, changed value, ...)
-			title: "Change value"          // Used as title on button, input, ...
+			action: function (value) {},        // Function that will be called with the value when status change (button press, stopped typing, changed value, ...)
+			title: "Change value",         // Used as title on button, input, ...
+			min: 1,                        // Minimum value for integers
+			max: 50                        // Maximum value for integer
 		}
